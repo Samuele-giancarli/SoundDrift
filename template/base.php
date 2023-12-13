2fe14acd4b16e2ee926e10c14d47e44080a81f49
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-12-13 10:46:41 +0100

    add person icon

--- template/base.php
+++ template/base.php
@@ -4,14 +4,21 @@
         <title><?php echo $templateParams["titolo"]; ?></title>
         <meta charset="UTF-8"/>
         <link href="css/style.css" type="text/css" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  </head>    
     <body class="bg-light">
         <header class="bg-dark d-flex justify-content-between align-items-center p-3">
             <h1 class="text-light">SoundRift</h1>
-            <button type="button" class="btn btn-primary">
-            <i class="bi bi-bell"></i>
-        </button>
+            <div>
+                <button type="button" class="btn btn-primary">
+                    <i class="bi bi-bell"></i>
+                </button>
+                <button type="button" class="btn btn-primary">
+                    <i class="bi bi-person"></i>
+                </button>
+            </div>
         </header>
         <main>
 
@@ -19,5 +26,6 @@
         <footer class="bg-dark">
             <p>SoundRift - A.A. 2023-2024</p>
         </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
