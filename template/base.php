7b71634b291d94995b66c7357c0ed31abada242f
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-12-13 10:22:08 +0100

    add bell icons

--- template/base.php
+++ template/base.php
@@ -7,15 +7,14 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>    
     <body class="bg-light">
-        <header class="bg-dark">
+        <header class="bg-dark d-flex justify-content-between align-items-center p-3">
             <h1 class="text-light">SoundRift</h1>
+            <button type="button" class="btn btn-primary">
+            <i class="bi bi-bell"></i>
+        </button>
         </header>
-        <nav>
-            <ul>
-                <li><a href="#">Opzione 1</a></li><li><a href="#">Opzione 2</a></li>
-            </ul>
-        </nav>
         <main>
+
         </main>
         <footer class="bg-dark">
             <p>SoundRift - A.A. 2023-2024</p>
