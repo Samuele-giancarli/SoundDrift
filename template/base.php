3b5266f097b24665be036597c37779ea8d27995e
Samuele-giancarli <samugianca@gmail.com>
2023-12-13 12:51:22 +0100

    doppio footer corretto

--- template/base.php
+++ template/base.php
@@ -7,7 +7,43 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="base.css">
+        <link rel="stylesheet" href="base2.css">
+
+        <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+        }
+
+        header {
+            background-color: #343a40;
+            color: #fff;
+            padding: 1rem;
+            text-align: center;
+        }
+
+        main {
+            flex: 1;
+        }
+
+        footer {
+            background-color: #343a40;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+
+        #second-footer {
+            position: relative; /* Modifica qui */
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+
     </head>    
     <body class="bg-light">
         <header class="bg-dark fixed-top d-flex justify-content-between align-items-center p-4">
@@ -24,10 +60,12 @@
         <main>
 
         </main>
-        <footer class="bg-secondary fixed-bottom p-3 text-center">
-            Scrivo qualcosa</footer>
-            <footer class="bg-dark fixed-bottom p-3 text-center">
-        <div class="container">
+        <footer class="bg-secondary p-3 text-center">
+            Scrivo qualcosa
+        </footer>
+        
+        <footer id="second-footer" class="bg-dark p-3 text-center">
+            <div class="container">
                 <button type="button" class="btn btn-primary">
                     <i class="bi bi-search"></i> Cerca
                 </button>
