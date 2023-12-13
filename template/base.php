5985603d8be9b3864529ee0570aebc2493263771
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-12-13 10:53:39 +0100

    change padding in header and footer

--- template/base.php
+++ template/base.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   </head>    
     <body class="bg-light">
-        <header class="bg-dark d-flex justify-content-between align-items-center p-3">
+        <header class="bg-dark d-flex justify-content-between align-items-center p-4">
             <h1 class="text-light">SoundRift</h1>
             <div>
                 <button type="button" class="btn btn-primary">
@@ -23,7 +23,7 @@
         <main>
 
         </main>
-        <footer class="bg-dark fixed-bottom">
+        <footer class="bg-dark fixed-bottom p-4">
             <p>SoundRift - A.A. 2023-2024</p>
         </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
