2a0cb856ac5c82d488db3b723d01413485a87862
Samuele-giancarli <samugianca@gmail.com>
2024-01-05 01:51:37 +0100

    trying to fix css

--- template/base.php
+++ template/base.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php echo $templateParams["titolo"]; ?></title>
         <meta charset="UTF-8"/>
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../SoundDrift/css/style.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Major Mono Display">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
