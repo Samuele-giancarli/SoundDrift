6926b26c020a1feffd6e7e1758814280799c42cb
Samuele-giancarli <samugianca@gmail.com>
2024-01-08 18:43:48 +0100

    working uploadForm

--- template/uploadForm.php
+++ template/uploadForm.php
@@ -3,19 +3,27 @@
     $err_mess = null;
     
     // Verifica se l'utente è già loggato
-    if (!isset($_SESSION["email"])) {
+    if (!isset($_SESSION["ID"])) {
         $err_mess = "Devi effettuare l'accesso per pubblicare un post.";
         echo $err_mess;
     } else if($_SERVER["REQUEST_METHOD"] === "POST") {
         try {
             $db = new mysqli("127.0.0.1", "root", "", "sounddrift", 3306);
             
-            $Email = $_SESSION["email"];
+            $Img;
+            $UserId = $_SESSION["ID"];
             $DatePost = date("Y-m-d H:i:s");
-            $Img = $_FILES["immagine"];
+            //$Img = $_FILES["immagine"];
+            //TODO: gestire l'inserimento immagine
             $Textual = $_POST["testo"];
 
-            $query = "INSERT INTO testuale (Email, Data, Immagine, Testo) VALUES
+            echo $UserId;
+            echo $DatePost;   
+            echo $Textual;
+
+
+
+            $query = "INSERT INTO post (Data, ID_Immagine, Testo, ID_Utente) VALUES
             (?, ?, ?, ?);";
 
             /* queste sono le informazioni che posso trarre da un'immagine
@@ -27,7 +35,7 @@
             */
 
             $stmt = $db->prepare($query);
-            $stmt->bind_param("ssss", $Email, $DatePost, $Img["name"], $Textual);
+            $stmt->bind_param("ssss", $DatePost, $Img, $Textual, $UserId);
             
             if (!$stmt->execute()) {
                 echo "Errore durante l'esecuzione della query: " . $stmt->error;
