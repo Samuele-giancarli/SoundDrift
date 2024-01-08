a63284c33c787aa41485b04ceb63d87d5806f515
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-08 22:32:27 +0100

    song upload and album create

--- template/albumCreate.php
+++ template/albumCreate.php
@@ -1,5 +1,6 @@
-
 <?php
+
+require_once("common.php");
 
 $show_form = true;
 $err_mess = null;
@@ -20,32 +21,18 @@ if(!isset($_SESSION["ID"])) {
         $db = new mysqli("127.0.0.1", "root", "", "sounddrift", 3306);
         $idimmagine=null;
         if ($immagine["error"]==0){
-            $null = null;
-            $imgname=$immagine["name"];
-            $mimetype=$immagine["type"];
-            $imgsize=$immagine["size"];
-            $fp = fopen($immagine["tmp_name"], "rb");
-            $content = fread($fp, $imgsize);
-            fclose($fp);
-            $stmt = $db->prepare("INSERT INTO risorsa(FileName,MimeType,Contenuto) VALUES(?,?,?)");
-            $stmt->bind_param("ssb", $imgname, $mimetype, $null);
-            $stmt->send_long_data(2, $content);
-            try {
-                if(!$stmt->execute()) {
-                    echo "Errore sconosciuto";
-                    die();
-                }
-            } catch(mysqli_sql_exception $e) {
-                echo "Errore generico (ex. dimensione eccessiva)";
+            $idimmagine=store_file($db,$immagine);
+            if (is_null($idimmagine)){
+                echo "Errore generico";
                 die();
             }
-            $idimmagine=$db->insert_id;
         }
         $stmt = $db->prepare("INSERT INTO album(Titolo,Genere,ID_Utente,Data,ID_Immagine) VALUES(?,?,?,CURDATE(),?)");
         $stmt->bind_param("ssii", $titolo, $genere, $idutente,$idimmagine);
         try {
             if($stmt->execute()) {
-                $err_mess="<a href=\"songUpload.php\" style=\"color:black\">L'album ".htmlentities($titolo)." è stato creato: aggiungi subito delle canzoni!</a>";
+                $err_mess="<a href=\"songUpload.php\" style=\"color:black\">L'album ".htmlentities($titolo)." è stato creato: aggiungi subito delle canzoni!</a><br>";
+                $err_mess=$err_mess."<a href=\"albumCreate.php\" style=\"color:black\">Oppure crea un nuovo album.</a>";
                 $show_form=false;
             } else {
                 $err_mess="Errore sconosciuto";
@@ -63,13 +50,30 @@ if ($show_form) {
 ?>
 <form method="POST" action="albumCreate.php" enctype="multipart/form-data">
     <input type="text" name="titolo" placeholder="Titolo dell'album"/>
+    <br>
     <input type="text" name="genere" placeholder="Genere dell'album"/>
-    <input type="file" name="immagine" accept="image/jpeg,image/png,image/webp,image/avif"/>
+    <br> 
+    Immagine: <input type="file" name="immagine" accept="image/jpeg,image/png,image/webp,image/avif"/>
+    <br>
     <input type="submit" value="Invia"/>
 </form>
 <?php
 }
 if(!is_null($err_mess)) {
     echo $err_mess;
+}
+if ($show_form){
+    $db = new mysqli("127.0.0.1", "root", "", "sounddrift", 3306);
+    echo "Album da finalizzare: <br> <ul>";
+    $stmt = $db->prepare("SELECT * FROM album WHERE Finalizzato=0 AND ID_Utente=?");
+    $idcreatore=$_SESSION["ID"];
+    $stmt->bind_param("i", $idcreatore);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()){
+        echo "<li>".htmlentities($row["Titolo"])." <button onclick=\"location.href='finalise.php?id=".$row["ID"]."'\" type=\"button\">Finalizza</button></li>";
+    }
+    echo "</ul>";
+    $db->close();
 }
 ?>
---
+++ template/songUpload.php
@@ -0,0 +1,92 @@
+<?php
+require_once("common.php");
+
+$show_form = true;
+$err_mess = null;
+
+$db = new mysqli("127.0.0.1", "root", "", "sounddrift", 3306);
+
+if(!isset($_SESSION["ID"])) {
+    $err_mess="Per caricare tuoi brani, devi essere loggato!";
+    $show_form=false;
+}elseif(isset($_POST["titolo"])){
+    $idutente=$_SESSION["ID"];
+    $titolo=$_POST["titolo"];
+    $genere=$_POST["genere"];
+    $immagine=$_FILES["immagine"];
+    $audio=$_FILES["audio"];
+    $idalbum=$_POST["album"];
+    if($idalbum == "null") {
+        $idalbum = null;
+    }
+    if (strlen($titolo)<=0||strlen($titolo)>=256){
+        $err_mess="Il titolo non ha lunghezza valida";
+    }elseif(strlen($genere)<=0||strlen($genere)>=256){
+        $err_mess="Il genere non ha lunghezza valida";
+    }elseif($audio["error"]!=0){
+        $err_mess="Non hai inserito il file audio";
+    }else{
+        $idimmagine=null;
+        $idaudio=store_file($db,$audio);
+        if (is_null($idaudio)){
+            echo "Errore generico";
+            die();
+        }
+        if ($immagine["error"]==0){
+            $idimmagine=store_file($db,$immagine);
+            if (is_null($idimmagine)){
+                echo "Errore generico";
+                die();
+            }
+        }
+        $stmt = $db->prepare("INSERT INTO canzone(Titolo,Genere,ID_Utente,Data,ID_Immagine,ID_Audio,ID_Album) VALUES(?,?,?,CURDATE(),?,?,?)");
+        $stmt->bind_param("ssiiii", $titolo, $genere, $idutente,$idimmagine,$idaudio,$idalbum);
+        try {
+            if($stmt->execute()) {
+                $err_mess="<a href=\"songUpload.php\" style=\"color:black\">La canzone ".htmlentities($titolo)." è stata caricata: aggiungi subito altre canzoni!</a><br>";
+                $show_form=false;
+            } else {
+                $err_mess="Errore sconosciuto";
+            }
+        } catch(mysqli_sql_exception $e) {
+            $err_mess="Errore generico";
+        }
+    }
+}
+?>
+
+<?php
+if ($show_form) {
+?>
+<form id="songupload" method="POST" action="songUpload.php" enctype="multipart/form-data">
+    <input type="text" name="titolo" placeholder="Titolo della canzone"/>
+    <br>
+    <input type="text" name="genere" placeholder="Genere della canzone"/>
+    <br>
+    Immagine: <input type="file" name="immagine" accept="image/jpeg,image/png,image/webp,image/avif"/>
+    <br>
+    File audio: <input type="file" name="audio" accept="audio/mpeg,audio/flac"/>
+    <br>
+    Album di appartenenza: <select name="album" form="songupload">
+        <option value="null">no album</option>
+<?php
+$stmt = $db->prepare("SELECT * FROM album WHERE Finalizzato=0 AND ID_Utente=?");
+$idcreatore=$_SESSION["ID"];
+$stmt->bind_param("i", $idcreatore);
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()){
+    echo "<option value=".$row["ID"].">".htmlentities($row["Titolo"])."</option>";
+}
+?>
+    </select>
+    <br>
+    <input type="submit" value="Invia"/>
+</form>
+<?php
+}
+if(!is_null($err_mess)) {
+    echo $err_mess;
+}
+$db->close();
+?>
