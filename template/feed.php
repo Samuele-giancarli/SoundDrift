4b8fa6cb943db83de6cdd4ffbfd0926bb548e685
Samuele-giancarli <samugianca@gmail.com>
2024-01-09 20:01:27 +0100

    working on feed graphics

--- template/feed.php
+++ template/feed.php
@@ -1,16 +1,32 @@
-<!-- <div class="post-box" color=black>
-    <?php
-        //prendo dalla tabella utente riga 0 il suo username
-        //echo $templateParams["post"][0]["Username"];
-        //echo $templateParams["utente"][1]["Username"];
-        //echo $templateParams["utente"][3]["Username"];
-
-        //DEVO CREARE LA QUERY GETPOST o GETPOSTLIST SU DATABASE:PHP        
-    ?>
-</div> -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <?php foreach ($templateParams["feedData"] as $post): ?>
-    <div class="post">
-        <p><?php echo $post['Testo']; ?></p>
+    <div class="row justify-content-center text-center">
+        <div class="col-md-6 text-center">
+            <div class="card rounded-3 text-center">
+                <div class="card-body">
+                    <h5 class="card-title"> <?php echo $post['Testo']; ?> </h5>
+                    <p class="card-text">
+                        <?php
+                            $postImage = $post['ID_Immagine']; 
+                            echo $postImage;     
+                        ?>
+                        <img <?php echo "src=\"".$postImage."\""; ?>  id="profile-pic" class="img-thumbnail" style="width: 150px; height: 150px;" />
+                        <br>
+                        <?php $imageID="download.php?id=".$postImage; ?> <br>
+                        <?php echo $post['Testo']; ?> <br>               
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">Like</button>
+                            <!-- immagini -->
+
+                            <button type="button" class="btn btn-secondary">Condividi</button>
+                        </div>
+                        <small class="text-muted">10 peni nel culo</small>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <?php endforeach; ?>
