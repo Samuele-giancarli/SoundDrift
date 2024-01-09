0a1ed2603d9f0c7d9ff1ecf6ae15acbb5d032ef0
Samuele-giancarli <samugianca@gmail.com>
2024-01-09 11:31:11 +0100

    working feed proto

--- database/database.php
+++ database/database.php
@@ -29,6 +29,8 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
     */
+
+    //è un getNumber
     public function getFollowingOfUser($ID){
         $query = "SELECT COUNT(*) as num_following FROM follow WHERE ID_Seguace = ?";
         $stmt = $this->db->prepare($query);
@@ -39,6 +41,7 @@ class DatabaseHelper{
         return $result->fetch_assoc();
     }
         
+    //è un getNumber
     public function getFollowerOfUser($ID){
         $query = "SELECT COUNT(*) as num_followers FROM follow WHERE ID_Seguito = ?";
         $stmt = $this->db->prepare($query);
@@ -48,6 +51,56 @@ class DatabaseHelper{
         
         return $result->fetch_assoc();
     }
+
+    //ritorna la lista di following dello user attuale
+    public function getListOfFollowing($ID){
+        $query = "SELECT ID_Seguito FROM follow WHERE ID_Seguace = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $ID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        $followingList = array(); // Inizializza un array per contenere la lista di following
+
+        while ($row = $result->fetch_assoc()) {
+            $followingList[] = $row['ID_Seguito'];
+        }
+
+        return $followingList;
+    }
+
+    public function getFeed($ID) {
+        $followingList = $this->getListOfFollowing($ID);
     
+        if (empty($followingList)) {
+            return array();
+        }
+    
+        $followingIDs = implode(",", $followingList);
+    
+        $query = "SELECT * FROM post WHERE ID_Utente IN ($followingIDs) ORDER BY Data DESC";
+        $result = $this->db->query($query);
+    
+        $feed = array();
+    
+        while ($row = $result->fetch_assoc()) {
+            $feed[] = $row;
+        }
+    
+        return $feed;
+    }
+    
+    
+
+    /*public function getFeed($ID){
+        $query = "SELECT * FROM post, utente Username";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $userID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }*/
 }
 ?>
