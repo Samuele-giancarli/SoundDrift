80adafca584463ea235f4ecb02c975cf1c100ecb
MarcoCostantini26 <marco.costantini26@gmail.com>
2024-01-09 13:12:09 +0100

    profile get

--- profile.php
+++ profile.php
@@ -5,7 +5,7 @@ require_once 'bootstrap.php';
 //Base Template
 $templateParams["titolo"] = "SoundDrift - Profilo";
 $templateParams["nome"] = "profile.php";
-$templateParams["utente"] = $dbh->getUser($_SESSION["ID"]);
+$templateParams["utente"] = $dbh->getUser($_GET["utenteCorrente"]);
 $templateParams["num_seguiti"] = $dbh->getFollowingOfUser($_SESSION["ID"]);
 $templateParams["num_seguaci"] = $dbh->getFollowerOfUser($_SESSION["ID"]);
 
