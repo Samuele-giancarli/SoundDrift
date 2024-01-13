92e7f635748b85d4862312e0f13586ddae36d36a
MarcoCostantini26 <marco.costantini26@gmail.com>
2024-01-13 18:28:49 +0100

    improve comment section

--- template/comment.php
+++ template/comment.php
@@ -13,6 +13,7 @@
 
     foreach ($comments as $comment) {
         echo '<div class="comment">';
+        echo '<p>' . $dbh->getUserInfo($comment['ID_Utente'])["Username"] . '</p>';
         echo '<p>' . $comment['Testo'] . '</p>';
         echo '</div>';
     }
@@ -34,14 +35,20 @@
         // Gestisci la risposta dalla richiesta
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+
                 // Aggiorna la visualizzazione dei commenti senza ricaricare la pagina
                 let commentsContainer = document.getElementById('commentsContainer');
-                commentsContainer.innerHTML = xhr.responseText;
+                let newComment = document.createElement('div');
+                newComment.className = 'comment';
+                newComment.innerHTML = '<p>' + response.username + '</p> <p>' + response.commentText + '</p>';
+                commentsContainer.appendChild(newComment);
 
                 // Pulisci il campo di testo del commento dopo l'invio
                 document.getElementById('commentText').value = '';
             }
-        }
+        };
+
         // Invia i dati nella richiesta
         xhr.send('postId=' + encodeURIComponent(postId) + '&commentText=' + encodeURIComponent(commentText));
     }
