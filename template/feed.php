284f28e9749de08f7bf243896db419a34d6e5a58
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-13 17:39:12 +0100

    feed links to search

--- template/feed.php
+++ template/feed.php
@@ -1,3 +1,8 @@
-<?php foreach ($templateParams["feedData"] as $post):
+<?php 
+if (isset($_SESSION["ID"])&&$templateParams["feedData"]==null){
+    echo "<a style=\"color:black; text-decoration:none\" href=\"ricerca.php?id=".$_SESSION["ID"]."\" > Comincia subito a seguire qualcuno! </a>";
+}
+
+foreach ($templateParams["feedData"] as $post):
     renderPost($post, $dbh, $templateParams["ID_Visualizer"]);
 endforeach; ?>
