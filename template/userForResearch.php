a85e8d01c20859738839216cc1b1249f30103a4c
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-18 16:57:13 +0100

    userForResearch

--- template/userForResearch.php
+++ template/userForResearch.php
@@ -10,13 +10,13 @@
     <div class="col-md-6 text-center"> 
         <div class="card rounded-3 text-center">
             <div class="card-body">
-                <h5 class="card-title">
+                <p class="h5 card-title">
                     <a href="profile.php?id=<?php echo $userid ?>" style="color: black"><?php echo $username. " (Utente)" ?></a>
-                </h5>
+    </p>
 
                 <p class="card-text" style="text-align:center;">
                     <?php if ($image != null) { ?>
-                        <img src="<?php echo $imagePath; ?>" id="profile-pic" class="img-thumbnail" style="width: 150px; height: 150px;" />
+                        <img src="<?php echo $imagePath; ?>" class="img-thumbnail" style="width: 150px; height: 150px;" />
                     <?php } ?>
                     <br>
                 </p>
