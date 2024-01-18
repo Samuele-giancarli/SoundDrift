ae1e45973e7513c6aab777c5903f39be3fe3487a
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-18 14:04:08 +0100

    base.php checked

--- template/base.php
+++ template/base.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="it">
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title><?php echo $templateParams["titolo"]; ?></title>
-        <meta charset="UTF-8"/>
-        <link rel="stylesheet" href="css/style.css" aria-atomic="">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Major Mono Display">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Soundrift - Base</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Major+Mono+Display">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -15,7 +15,7 @@
         <header class="bg-black fixed-top d-flex justify-content-between align-items-center header-content">
 
             <a href="home.php" id="logo">
-                    <img src="images/App_images/logo3.jpg" width="70" style="display:inline-block">
+                    <img src="images/App_images/logo3.jpg" alt="logo" width="70" style="display:inline-block">
                     <h1 class="text-white" style="display:inline-block; font-size:27px" >SoundDrift</h1>
             </a>
 
@@ -23,48 +23,48 @@
 
             <?php
                 if(isset($_SESSION["ID"])) {
-                ?>
+            ?>
                     <div class="btn-group" id="upperButtons">
                         <a id="home" href="home.php" class="btn btn-dark " style="text-decoration:none">
-                            <i class="bi bi-house" style="font-size: 20px"></i>
+                            <em class="bi bi-house" style="font-size: 20px"></em>
                         </a>
                         <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             Carica
                         </button>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="songUpload.php"><i class="bi bi-plus-square"></i> Carica Canzone</a></li>
-                            <li><a class="dropdown-item" href="albumCreate.php"><i class="bi bi-disc-fill"></i> Crea Album</a></li>
-                            <li><a class="dropdown-item" href="playlist.php"><i class="bi bi-cassette"></i> Crea Playlist</a></li>
+                            <li><a class="dropdown-item" href="songUpload.php"><em class="bi bi-plus-square"></em> Carica Canzone</a></li>
+                            <li><a class="dropdown-item" href="albumCreate.php"><em class="bi bi-disc-fill"></em> Crea Album</a></li>
+                            <li><a class="dropdown-item" href="playlist.php"><em class="bi bi-cassette"></em> Crea Playlist</a></li>
                         </ul>
                         <a href="profile.php?id=<?php echo $_SESSION["ID"] ?>" class="btn btn-dark" style="text-decoration:none">
-                            <i class="bi bi-person" style="font-size: 20px"></i>
+                            <em class="bi bi-person" style="font-size: 20px"></em>
                         </a>
 
                         <?php
                         if ($dbh->newNotifications($_SESSION["ID"])){
                             ?>
                             <a href="notifications.php?id=<?php echo $_SESSION["ID"] ?>" class="btn btn-danger" style="text-decoration:none">
-                            <i class="bi bi-bell" style="font-size: 20px"></i>
+                            <em class="bi bi-bell" style="font-size: 20px"></em>
                         </a>
                         <?php
                         }else{
                             ?>
                         <a href="notifications.php?id=<?php echo $_SESSION["ID"] ?>" class="btn btn-dark" style="text-decoration:none">
-                            <i class="bi bi-bell" style="font-size: 20px"></i>
+                            <em class="bi bi-bell" style="font-size: 20px"></em>
                         </a>
                         <?php
                         }
                         
                         ?>
                         <a href="logout.php" class="btn btn-dark" style="text-decoration:none">
-                            <i class="bi bi-box-arrow-left" style="font-size: 20px"></i>
+                            <em class="bi bi-box-arrow-left" style="font-size: 20px"></em>
                         </a>
                     </div>
                 <?php
                 } else {
                 ?>
-                    <a id="loginfromOut" href="login.php" class="btn btn-dark">
-                        <i class="bi bi-box-arrow-in-right" style="font-size: 20px"></i>
+                    <a id="loginfromOut" href="login.php" class="btn btn-dark">Login
+                        <em class="bi bi-box-arrow-in-right" style="font-size: 20px"></em>
                     </a>
                 <?php
                 }
@@ -86,18 +86,18 @@
         <footer id="second-footer" class="bg-black text-center z-3">
             <div class="container">
                 <a href="ricerca.php" class="btn btn-dark " style="text-decoration:none">
-                        <i class="bi bi-search"></i> <span class="hide-on-mobile">Cerca</span>
+                        <em class="bi bi-search"></em> <span class="hide-on-mobile">Cerca</span>
                 </a>
                 <a href="libreria.php" class="btn btn-dark" style="text-decoration:none">
-                        <i class="bi bi-collection"></i> Libreria</span>
+                        <em class="bi bi-collection"></em> Libreria</span>
                 </a>
                 </a>
                 <a href="upload.php" class="btn btn-dark" style="text-decoration:none">
-                        <i class="bi bi-file-post"></i> Post</span>
+                        <em class="bi bi-file-post"></em> Post</span>
                 </a>
                 </a>
                 <a href="settings.php" class="btn btn-dark" style="text-decoration:none">
-                        <i class="bi bi-gear"></i> <span class="hide-on-mobile">Impostazioni</span>
+                        <em class="bi bi-gear"></em> <span class="hide-on-mobile">Impostazioni</span>
                 </a>
                 </a>
             </div>
