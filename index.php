047250618fa3662d33e851dd115147cac5ad5d5f
MarcoCostantini26 <marco.costantini26@gmail.com>
2024-01-18 14:03:13 +0100

    fix checkers html and achecker in index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="it">
 <head>
     <title>SoundDrift</title>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="css/style.css" aria-atomic="">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Major Mono Display">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Major%20Mono%20Display">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -15,16 +15,18 @@
 </head>
 <body>
 <audio id="audio"></audio>
-<iframe src="home.php" width="100%" height="100%"></iframe>
+<iframe src="home.php"></iframe>
 <footer class="bg-opacity-10 bg-secondary text-center">
-<span class="hide-on-mobile"><input type="range" min="0" max="1" value="1" step="0.01" id="volume" onchange="volumeUpdate();" oninput="volumeUpdate();"></input></span>
-
+<span class="hide-on-mobile">
+  <label for="volume" style="display: none;">Volume:</label>
+  <input type="range" min="0" max="1" value="1" step="0.01" id="volume" onchange="volumeUpdate();" oninput="volumeUpdate();">
+</span>
         <button type="button" class="btn btn-dark" id="playButton">
-            <i class="bi bi-play"></i> <span class="hide-on-mobile">Play</span>
+            <em class="bi bi-play"></em> <span class="hide-on-mobile">Play</span>
         </button>
     
         <button type="button" class="btn btn-dark" onclick="prevSong();">
-            <i class="bi bi-skip-backward"></i>
+            <em class="bi bi-skip-backward"></em>
         </button>
 
         <div class="dropup-center dropup" style="display:inline-block">
@@ -38,21 +40,19 @@
         </ul>
 </div>
         <button type="button"  class="btn btn-dark" onclick="nextSong();">
-            <i class="bi bi-skip-forward"></i> 
+            <em class="bi bi-skip-forward"></em> 
         </button>
 
         <span class="hide-on-mobile"><button type="button" class="btn btn-dark" id="loopButton" onclick="enableLoop();">
-            <i class="bi bi-arrow-repeat"></i> Enable Loop
+            <em class="bi bi-arrow-repeat"></em> Enable Loop
         </button></span>
 
-        <div class="dropup-center dropup" style="display:inline-block">
-        <span class="hide-on-mobile"> 
+        <div class="dropup-center dropup hide-on-mobile" style="display:inline-block">
         <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Prossimo in coda</button>
         <ul class="dropdown-menu">
-        <li a href="#" id="next" style="text-align:center"></a></li>
+        <li> <a href="#" id="next" style="text-align:center" title="Next Page"></a></li>
         </ul>
         </div>
-        </span>
 </footer>
 </body>
 </html>
