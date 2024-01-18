0ecc23777fe54b6ce7146c3e9f27cbeb32edc545
Samuele-giancarli <samugianca@gmail.com>
2024-01-18 17:30:43 +0100

    fix in error messages, login

--- template/loginForm.php
+++ template/loginForm.php
@@ -4,6 +4,9 @@ $show_form = false;
 $err_mess=null;
 $success=null;
 
+$err_password = false;
+$err_utente = false;
+
 if(isset($_SESSION["ID"])) {
     $err_mess="L'utente è già loggato";
 } elseif(isset($_POST["email"])) {
@@ -15,12 +18,15 @@ if(isset($_SESSION["ID"])) {
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
     $db->close();
+
     if(is_null($row)) {
         $show_form = true;
-        $err_mess="L'utente non esiste";
+        //$err_mess="L'utente non esiste";
+        $err_utente = true;
     } elseif(!password_verify($_POST["password"], $row["Password"])) {
         $show_form = true;
-        $err_mess= "Password errata";
+        //$err_mess= "Password errata";
+        $err_password = true;
     } else {
         $_SESSION["ID"] = $row["ID"];
         $_SESSION["username"] = $row["Username"];
@@ -56,13 +62,13 @@ if($show_form):
 <?php endif; ?>
 <?php
 
-if (!is_null($err_mess)) { ?>
+if ($err_utente) { ?>
     <div class="alert alert-danger mt-4">
-        <?php echo $err_mess; ?>
+        <?php echo "utente non trovato"; ?>
     </div>
-<?php } elseif (!is_null($success)){ ?>
-    <div class="alert alert-primary mt-4">
-    <?php echo $success; ?>
+<?php } elseif ($err_password){ ?>
+    <div class="alert alert-danger mt-4">
+    <?php echo "password errata"; ?>
     </div>
 <?php } 
 
