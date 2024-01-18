6407bde2bf33fcc4cbb6c6a73c20ab68b76ef3ff
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-18 16:40:05 +0100

    libreria

--- template/libreria.php
+++ template/libreria.php
@@ -8,25 +8,25 @@ $idutente=$_SESSION["ID"];
 
 <div class="mb-3">
 <a href="braniPiaciuti.php" class="btn btn-dark" style="text-decoration:none">
-    <i class="bi bi-heart" style="font-size: 20px"> Vai ai brani che ti piacciono</i>
+    <em class="bi bi-heart" style="font-size: 20px"> Vai ai brani che ti piacciono</em>
   </a>
 </div>
 
 <div class="mb-3">
 <a href="albumPiaciuti.php" class="btn btn-dark" style="text-decoration:none">
-    <i class="bi bi-disc" style="font-size: 20px"> Vai agli album salvati</i>
+    <em class="bi bi-disc" style="font-size: 20px"> Vai agli album salvati</em>
   </a>
 </div>
 
 <div class="mb-3">
 <a href="playlistPiaciute.php" class="btn btn-dark" style="text-decoration:none">
-    <i class="bi bi-cassette" style="font-size: 20px"> Vai alle playlist salvate</i>
+    <em class="bi bi-cassette" style="font-size: 20px"> Vai alle playlist salvate</em>
   </a>
 </div>
 
 <div class="mb-3">
 <a href="playlist.php" class="btn btn-dark" style="text-decoration:none">
-    <i class="bi bi-music-note-list" style="font-size: 20px"> Crea una playlist</i>
+    <em class="bi bi-music-note-list" style="font-size: 20px"> Crea una playlist</em>
   </a>
 </div>
 
