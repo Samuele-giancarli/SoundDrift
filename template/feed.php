37a9e7b97824ed751a72acbcf1448253323ce129
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-18 17:02:33 +0100

    feed fix

--- template/feed.php
+++ template/feed.php
@@ -1,6 +1,6 @@
 <?php 
 if (isset($_SESSION["ID"]) && $templateParams["feedData"] == null){
-    echo '<div class="d-flex justify-content-center align-items-center";">';
+    echo '<div class="d-flex justify-content-center align-items-center">';
     echo '<a class="btn btn-dark" href="ricerca.php?id='.$_SESSION["ID"].'">Comincia subito a seguire qualcuno!</a>';
     echo '</div>';
 }
