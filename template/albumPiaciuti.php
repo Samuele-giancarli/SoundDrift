04c44136a6b3c7221bce951331c3a9cfc89ef45d
Aisja Baglioni <aisjabaglioni@gmail.com>
2024-01-18 16:33:20 +0100

    albumPiaciuti

--- template/albumPiaciuti.php
+++ template/albumPiaciuti.php
@@ -20,12 +20,12 @@ if (!is_null(count($rows))){
         if (!is_null($idimmagine)){
             echo "<li class=\"list-group-item\"> <a title=\"album\" class=\"link-primary\" href=\"albumPlayer.php?id=".$albumInfo["ID"]."\">".$albumInfo["Titolo"]."</a>".
             " - "."<a title=\"username\" style=\"color:black\" href=\"profile.php?id=".$authorInfo["ID"]."\">".$authorInfo["Username"]."</a>"
-            ."<img class=\"img-thumbnail\" id=\"profile-pic\" src=\"download.php?id=".$idimmagine."\" style=\"width: 75px; height: 75px\"></li>";
+            ."<img alt=\"albumCover\" class=\"img-thumbnail\" src=\"download.php?id=".$idimmagine."\" style=\"width: 75px; height: 75px\"></li>";
              }else{
             $idimmagine="default-cover.png";
             echo "<li class=\"list-group-item\"> <a title=\"albumInfo\" class=\"link-primary\" href=\"albumPlayer.php?id=".$albumInfo["ID"]."\">".$albumInfo["Titolo"]."</a>".
             " - "."<a title=\"authorinfo\" style=\"color:black\" href=\"profile.php?id=".$authorInfo["ID"]."\">".$authorInfo["Username"]."</a>"
-            ."<img alt=\"albumCover\" class=\"img-thumbnail\" id=\"profile-pic\" src=\"download.php?id=".$idimmagine."\" style=\"width: 75px; height: 75px\"></li>";
+            ."<img alt=\"albumCover\" class=\"img-thumbnail\" src=\"download.php?id=".$idimmagine."\" style=\"width: 75px; height: 75px\"></li>";
         }
 }
 }
